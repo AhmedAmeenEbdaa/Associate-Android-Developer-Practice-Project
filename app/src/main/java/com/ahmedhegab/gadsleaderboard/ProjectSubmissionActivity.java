3cacd2a4d814f26e1f96cda797a845f39da8499d
Ahmed Ameen <ahegab@ntgclarity.com>
2020-09-11 13:12:33 +0200

    third comment

--- app/src/main/java/com/ahmedhegab/gadsleaderboard/ProjectSubmissionActivity.java
+++ app/src/main/java/com/ahmedhegab/gadsleaderboard/ProjectSubmissionActivity.java
@@ -2,20 +2,150 @@ package com.ahmedhegab.gadsleaderboard;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.ahmedhegab.gadsleaderboard.retrofit.APIInterface;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class ProjectSubmissionActivity extends AppCompatActivity {
 
+    Button submit;
+    EditText firstName,lastName,email,link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_submission);
+
+        submit = findViewById(R.id.submit_project);
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+        link = findViewById(R.id.link);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+    }
+
+    private void showCustomDialog() {
+
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+
+        //then we will inflate the custom alert dialog xml that we created
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.confirmation_dialog, viewGroup, false);
+
+
+        //Now we need an AlertDialog.Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //setting the view of the builder to our custom view that we already inflated
+        builder.setView(dialogView);
+
+        //finally creating the alert dialog and displaying it
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+
+        ImageView close =  dialogView.findViewById(R.id.close);
+        Button ok =  dialogView.findViewById(R.id.buttonOk);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.hide();
+            }
+        });
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeRequest();
+            }
+        });
+    }
+
+    private void makeRequest() {
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://docs.google.com/forms/d/e/")
+                        .build();
+                APIInterface webservice = retrofit.create(APIInterface.class);
+                Call<Void> call = webservice.postValues(email.getText().toString(), firstName.getText().toString(), lastName.getText().toString(), link.getText().toString());
+
+
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        int responseCode = response.code();
+                        if (responseCode == 200) {
+
+                        showSuccessDialog();
+                        }else{
+                            showFailureDialog();
+
+                        }
+                    }
+
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        showFailureDialog();
+                    }
+
+
+                });
+
+    }
+
+    private void showFailureDialog() {
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+
+        //then we will inflate the custom alert dialog xml that we created
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.failure_dialog, viewGroup, false);
+
+
+        //Now we need an AlertDialog.Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //setting the view of the builder to our custom view that we already inflated
+        builder.setView(dialogView);
+
+        //finally creating the alert dialog and displaying it
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    private void showSuccessDialog() {
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+
+        //then we will inflate the custom alert dialog xml that we created
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.success_dialog, viewGroup, false);
+
+
+        //Now we need an AlertDialog.Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //setting the view of the builder to our custom view that we already inflated
+        builder.setView(dialogView);
+
+        //finally creating the alert dialog and displaying it
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
 
     }
 }
--- app/src/main/java/com/ahmedhegab/gadsleaderboard/retrofit/APIInterface.java
+++ app/src/main/java/com/ahmedhegab/gadsleaderboard/retrofit/APIInterface.java
@@ -3,7 +3,11 @@ package com.ahmedhegab.gadsleaderboard.retrofit;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
 
 public interface APIInterface {
     @GET("api/hours")
@@ -13,7 +17,11 @@ public interface APIInterface {
     Call<List<SkillIQLeader>> skillIQLeaderCall();
 
 
-//    @FormUrlEncoded
-//    @POST("/api/users?")
-//    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+    @FormUrlEncoded
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    Call<Void> postValues(@Field("entry.1824927963") String emailAddress,
+                           @Field("entry.1877115667") String firstName,
+                           @Field("entry.2006916086") String lastName,
+                           @Field("entry.284483984") String projectLink
+    );
 }
